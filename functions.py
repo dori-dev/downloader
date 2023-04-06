a143cf2e070c01417614d1d023bfc7558237e04b
dori-dev <mr.dori.dev@gmail.com>
2023-04-06 19:32:51 +0330

    get headers of url

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import aiohttp
+
+
 UNITS = ["", "K", "M", "G", "T", "P", "E", "Z"]
 
 
@@ -10,3 +13,12 @@ def format_size(number: int, suffix: str = "B") -> str:
             return f"{number:.1f} {unit}{suffix}"
         number /= 1024.0
     return f"{number:.1f} Y{suffix}"
+
+
+async def get_headers(url: str) -> dict:
+    """
+    Send head request to the specified url and return the headers.
+    """
+    async with aiohttp.ClientSession() as session:
+        async with session.head(url, allow_redirects=True) as response:
+            return dict(response.headers)
